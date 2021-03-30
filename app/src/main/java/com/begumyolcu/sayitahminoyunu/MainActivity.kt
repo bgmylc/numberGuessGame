6b0df9a59950ee5151a50f28a0f40bae64f4872f
bgmylc <begum.yolcu@tedu.edu.tr>
2021-03-30 15:30:21 +0300

    Text clear update

--- app/src/main/java/com/begumyolcu/sayitahminoyunu/MainActivity.kt
+++ app/src/main/java/com/begumyolcu/sayitahminoyunu/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.begumyolcu.sayitahminoyunu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 import kotlin.random.Random
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val randomSayi = Random.nextInt(1,101)
+        val randomSayi = Random.nextInt(1, 101)
         var score = 100
         buttonGuess.setOnClickListener {
             val guess = Integer.parseInt(numberText.text.toString())
@@ -22,12 +23,30 @@ class MainActivity : AppCompatActivity() {
                 textViewScore.visibility = View.INVISIBLE
                 textViewAlert.setText(alertString)
                 numberText.setFocusable(false)
+                buttonReplay.visibility = View.VISIBLE
+                buttonReplay.setOnClickListener {
+                    val intent = intent
+                    finish()
+                    overridePendingTransition(0, 0);
+                    startActivity(intent)
+                    overridePendingTransition(0, 0);
+
+                }
             }
             else if (guess != randomSayi && score == 1) {
                 score -= 1
                 val alertString = "Üzgünüm, oyunu kaybettiniz. Puanınız: 0"
                 textViewAlert.setText(alertString)
                 numberText.setFocusable(false)
+                buttonReplay.visibility = View.VISIBLE
+                buttonReplay.setOnClickListener {
+                    val intent = intent
+                    finish()
+                    overridePendingTransition(0, 0);
+                    startActivity(intent)
+                    overridePendingTransition(0, 0);
+
+                }
             }
             else if (guess < randomSayi){
                 val alertString = "Lütfen daha büyük bir sayı deneyin"
@@ -44,7 +63,17 @@ class MainActivity : AppCompatActivity() {
                 textViewScore.setText(scoreString)
             }
         }
-
+        numberText.setOnClickListener {
+          onClick(numberText)
+        }
+    }
+    fun onClick(v: View?) {
+        if (v==numberText) {
+            numberText.text?.clear()
+        }
     }
 }
+
+
+
 
